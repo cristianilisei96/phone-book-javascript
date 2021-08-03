23c779e070205220e34aa552e5d77a4caedd7b0b
Cristian Ilisei <cristianilisei96@gmail.com>
2021-08-04 02:36:52 +0300

    almost done

--- app.js
+++ app.js
@@ -1,86 +1,123 @@
-// get buttons from DOM
-const addNewContact = document.querySelector('button.btnAddNewContact');
-const deleteNewContact = document.querySelector('button.btnDeleteNewContact');
+// get buttons from html
+var addItemButton = document.querySelector('button.addNewStudent');
+var sortAscButton = document.querySelector('button.sortAscButton');
+var sortDescButton = document.querySelector('button.sortDescButton');
 
-// const
-const phoneBook = document.querySelector('.phoneBook');
-const list = document.querySelector('.tBodyList');
+// get input to add new student
+var addNewStudentInput = document.querySelector('.addNewStudentInput');
 
-const inputs = [
-    document.getElementById('inputName'),
-    document.getElementById('inputNumber')
-];
+// get hide row and student list
+var row = document.querySelector('.d-none');
+var list = document.querySelector('tbody.student-list');
 
-// add event listener 
-inputs.forEach((input) => {
-    input.addEventListener('keydown', handler);
-    input.addEventListener('input', handler);
-    input.addEventListener('keypress', submitOnEnterKey);
-})
-addNewContact.addEventListener('click', addNewContactFunction);
+// add event listener to buttons
+addItemButton.addEventListener('click', addNewStudentFunction);
+sortAscButton.addEventListener('click', sortAscFunction);
+sortDescButton.addEventListener('click', sortDescFunction);
 
-function checkLength(target) {
-    return !!target.value;
-}
+// var elements table
+var table, rows, switching, i, x, y, shouldSwitch;
+table = document.getElementById("myTable");
 
-function submitOnEnterKey(e) {
-    if (e.keyCode === 13) {
-        addNewContactFunction(e);
-        if (e.cancelable) {
-            e.preventDefault();
+function sortAscFunction() {
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[0];
+        y = rows[i + 1].getElementsByTagName("TD")[0];
+        // Check if the two rows should switch place:
+        if (dir == "asc") {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+                shouldSwitch = true;
+            break;
+            }
         }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
     }
 }
 
-function handler(e) {
-    if (e.type === 'input') {
-        validate(e.target);
-    } else {
-        submitOnEnterKey(e);
+function sortDescFunction(){
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "desc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[0];
+        y = rows[i + 1].getElementsByTagName("TD")[0];
+        // Check if the two rows should switch place:
+        if (dir == "desc") {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+                shouldSwitch = true;
+            break;
+            }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
     }
 }
 
-function validate(target) {
-    const result = checkLength(target);
-
-    if (result) {
-        target.classList.remove('is-invalid');
+function addNewStudentFunction(e){
+    if( addNewStudentInput.value.length == 0) {
+        addNewStudentInput.classList.add("is-invalid");
     } else {
-        target.classList.add('is-invalid');
-    }
-
-    return result;
-}
-
-function addNewContactFunction(e){
-    const result = inputs.map((input) => validate(input)).every((val) => val);
-
-    if(result) {
-        const tr = document.createElement('tr');
-        tr.classList.add('newTr');
-        tr.innerHTML = 
-            "<td>" + inputs[0].value + "</td>" + 
-            "<td>" + inputs[1].value + "</td>" + 
-            "<td><button class='btn btn-warning'>Modifica</button>" + "</td>" + 
-            "<td><button onclick='deleteContact(event)' class='btn btn-danger btnDeleteNewContact'>Sterge</button></td>";
-        phoneBook.classList.add('d-block');
-        const appendedItem = list.appendChild(tr);
-        inputs[0].value = '';
-        inputs[1].value = '';
-    } else {
+        
+        var tr = document.createElement('tr');
+        tr.innerHTML =  
+        "<td>" + addNewStudentInput.value + "</td>" +
+        "<td>" + "</td>" +
+        "<td>" + "</td>" +
+        "<td>" + 
+        "<button class='btn btn-primary btn-danger btn-buyed' onclick='deleteStudent(event)'>X</button>" + 
+        "</td>";
+        row.classList.add('d-block');
+        var appendedItem = list.appendChild(tr);
+        
+        addNewStudentInput.classList.remove("is-invalid");
+        addNewStudentInput.value = '';
+        
     }
     e.preventDefault();
 }
 
-function deleteContact(){
-    // console.log(event.target.parentElement.parentElement);
-    const contactToBeRemoved = event.target.parentElement.parentElement;
-    list.removeChild(contactToBeRemoved);
-    const newTr = document.querySelector('.newTr');
-    if(!list.contains(newTr)){
-        phoneBook.classList.remove('d-block');
-        // console.log('The list contains tr');
-    } else {
-        // console.log('The list does not contains any tr');
-    }
+function deleteStudent(){
+  console.log(event.target.parentElement.parentElement);
+  var studentToBeRemoved = event.target.parentElement.parentElement;
+  list.removeChild(studentToBeRemoved);
 }
